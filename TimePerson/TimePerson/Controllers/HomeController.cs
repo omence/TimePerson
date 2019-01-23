8f4d0c1029808843bd80dcf9af4f0c648e2a9854
Jason Few <omence11@gmail.com>
2019-01-23 08:41:57 -0800

    Initial Push

---
+++ TimePerson/TimePerson/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TimePerson.Models;
+
+namespace TimePerson.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int beginYear, int endYear)
+        {
+            return RedirectToAction("Results", new { beginYear, endYear });
+        }
+
+        public IActionResult Results(int beginYear, int endYear)
+        {
+
+
+        }
+    }
+}
