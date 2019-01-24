c379055cc7d88d59ae72446e12b6c33b0b3092bd
Jason Few <omence11@gmail.com>
2019-01-24 12:34:37 -0800

    Made comments and added link for home on results page

--- TimePerson/TimePerson/Controllers/HomeController.cs
+++ TimePerson/TimePerson/Controllers/HomeController.cs
@@ -5,20 +5,35 @@ using TimePerson.Models;
 
 namespace TimePerson.Controllers
 {
+    
     public class HomeController : Controller
     {
-        
+        /// <summary>
+        /// Sends index page to the browser
+        /// </summary>
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// gets input from the user and sends it to the Results method
+        /// </summary>
+        /// <param name="beginYear"></param>
+        /// <param name="endYear"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult Index(int beginYear, int endYear)
         {
             return RedirectToAction("Results", new { beginYear, endYear });
         }
 
+        /// <summary>
+        /// Calls method from TimePerson class and sends results to results html
+        /// </summary>
+        /// <param name="beginYear"></param>
+        /// <param name="endYear"></param>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Results(int beginYear, int endYear)
         {
